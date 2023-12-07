f0528184981e4ea601649aebf0c096f23c7fa930
ganyariya <ganariya2525@gmail.com>
2023-12-08 08:50:08 +0900

    :sparkles: X 実行時に yank されないようにする

--- dot_config/astronvim/lua/user/mappings.lua
+++ dot_config/astronvim/lua/user/mappings.lua
@@ -3,46 +3,49 @@
 -- Please use this mappings table to set keyboard mapping since this is the
 -- lower level configuration and more robust one. (which-key will
 -- automatically pick-up stored data by this setting.)
+
 return {
-  -- first key is the mode
-  -- normal mode
-  n = {
-    -- second key is the lefthand side of the map
+	-- first key is the mode
+	-- normal mode
+	n = {
+		-- second key is the lefthand side of the map
 
-    -- navigate buffer tabs with `H` and `L`
-    -- L = {
-    --   function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
-    --   desc = "Next buffer",
-    -- },
-    -- H = {
-    --   function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
-    --   desc = "Previous buffer",
-    -- },
+		-- navigate buffer tabs with `H` and `L`
+		-- L = {
+		--   function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
+		--   desc = "Next buffer",
+		-- },
+		-- H = {
+		--   function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
+		--   desc = "Previous buffer",
+		-- },
 
-    -- mappings seen under group name "Buffer"
-    ["<leader>bD"] = {
-      function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr)
-          require("astronvim.utils.buffer").close(bufnr)
-        end)
-      end,
-      desc = "Pick to close",
-    },
-    -- tables with the `name` key will be registered with which-key if it's installed
-    -- this is useful for naming menus
-    ["<leader>b"] = { name = "Buffers" },
-    -- quick save
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
-    -- ["<leader>a"] = { "<cmd> echo 'Hello' <cr>", desc="Hello, Worlddayo"}
-  },
+		-- mappings seen under group name "Buffer"
+		["<leader>bD"] = {
+			function()
+				require("astronvim.utils.status").heirline.buffer_picker(function(bufnr)
+					require("astronvim.utils.buffer").close(bufnr)
+				end)
+			end,
+			desc = "Pick to close",
+		},
+		-- tables with the `name` key will be registered with which-key if it's installed
+		-- this is useful for naming menus
+		["<leader>b"] = { name = "Buffers" },
+		-- quick save
+		-- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+		-- ["<leader>a"] = { "<cmd> echo 'Hello' <cr>", desc="Hello, Worlddayo"}
 
-  -- terminal mode
-  t = {
-    -- setting a mapping to false will disable it
-    -- ["<esc>"] = false,
-  },
-  -- insert mode
-  i = {},
-  -- visual mode
-  v = {},
+		["x"] = { '"_x', desc = "do not yank when delete", noremap = true, silent = true },
+	},
+
+	-- terminal mode
+	t = {
+		-- setting a mapping to false will disable it
+		-- ["<esc>"] = false,
+	},
+	-- insert mode
+	i = {},
+	-- visual mode
+	v = {},
 }
