837b8740e8573054ce164baf5fedcc98ca5ee91b
ganyariya <ganariya2525@gmail.com>
2024-07-15 17:53:29 +0900

    keymaps.lua を追加する

---
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,6 @@
+-- https://uhoho.hatenablog.jp/entry/2023/05/19/124317
+
+-- ここに keymapping を定義していく
+-- keymaps.lua 自体は init.lua で require() している
+vim.keymap.set('n', '<Leader>ev', '<Cmd>e ~/.config/nvim/init.lua<CR>')
+
