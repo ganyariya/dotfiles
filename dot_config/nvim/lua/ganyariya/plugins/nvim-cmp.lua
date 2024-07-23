fdf4806ad04a7c5e8858925a4cae8d0b55add3f8
ganyariya <ganariya2525@gmail.com>
2024-07-24 08:59:42 +0900

    :sparkles: Add bufferline

---
+++ dot_config/nvim/lua/ganyariya/plugins/nvim-cmp.lua
@@ -0,0 +1,17 @@
+return {
+  "hrsh7th/nvim-cmp",
+  -- Insert を押したらプラグインが Load される
+  event = "InsertEnter",
+  dependencies = {
+    "hrsh7th/cmp-buffer", -- source for text in buffer
+    "hrsh7th/cmp-path", -- source for file system paths
+    {
+      "L3MON4D3/LuaSnip",
+      version = "v2.*",
+      build = "make install_jsregexp",
+    },
+    "saadparwaiz1/cmp_luasnip", -- for autocompletion
+    "rafamadriz/friendly-snippets", -- useful snippets
+    "onsails/lspkind.nvim", -- vs-code like pictograms
+  },
+}
