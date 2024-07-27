fab919a1f996d16c5755675a24abfde6aa239247
ganyariya <ganariya2525@gmail.com>
2024-07-27 17:10:58 +0900

    Add telescope & which-key

---
+++ dot_config/nvim/lua/ganyariya/plugins/telescope.lua
@@ -0,0 +1,31 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  branch = "0.1.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    local telescope = require('telescope')
+    local actions = require('telescope.actions')
+    local builtin = require('telescope.builtin')
+
+    telescope.load_extension("fzf")
+
+    telescope.setup({
+      defaults = {
+        path_display = { "smart" },
+      },
+    })
+
+    local keymap = vim.keymap
+
+    -- keymap.set("n", "<Leader>ff", "<Cmd>Telescope find_files<CR>")
+    keymap.set("n", "<Leader>f?", builtin.builtin, { desc = "Telescope Lancher"})
+    keymap.set("n", "<Leader>ff", builtin.find_files, { desc = "Telescope Find Files" })
+    keymap.set("n", "<Leader>fr", builtin.oldfiles, { desc = "Telescope Recent Files" })
+    keymap.set("n", "<Leader>fk", builtin.keymaps, { desc = "Telescope KeyMaps" })
+    keymap.set("n", "<Leader>fs", builtin.live_grep, { desc = "Telescope Grep Texts in Files" })
+  end
+}
