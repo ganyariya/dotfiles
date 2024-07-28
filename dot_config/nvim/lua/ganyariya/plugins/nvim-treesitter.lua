b789e9a345ffcde99f1d056026f7dea79f5c7728
ganyariya <ganariya2525@gmail.com>
2024-07-28 13:38:19 +0900

    :sparkles: Custom treesitter config

--- dot_config/nvim/lua/ganyariya/plugins/nvim-treesitter.lua
+++ dot_config/nvim/lua/ganyariya/plugins/nvim-treesitter.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
+  lazy = false,
   build = ":TSUpdate",
   dependencies = {
     "windwp/nvim-ts-autotag",
@@ -22,35 +22,10 @@ return {
       },
       -- ensure these language parsers are installed
       ensure_installed = {
-        "json",
-        "javascript",
-        "typescript",
-        "tsx",
-        "yaml",
-        "html",
-        "css",
-        "prisma",
-        "markdown",
-        "markdown_inline",
-        "svelte",
-        "graphql",
-        "bash",
         "lua",
-        "vim",
-        "dockerfile",
-        "gitignore",
-        "query",
-        "vimdoc",
-        "c",
       },
       incremental_selection = {
         enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
       },
     })
   end,
