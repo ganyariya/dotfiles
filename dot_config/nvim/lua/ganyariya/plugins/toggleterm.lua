ab2c36ce4dc231ff69c39ff5bbcbbcf209ba844f
ganyariya <ganariya2525@gmail.com>
2024-08-04 12:10:04 +0900

    :sparkles: Toggle term を設定する

--- dot_config/nvim/lua/ganyariya/plugins/toggleterm.lua
+++ dot_config/nvim/lua/ganyariya/plugins/toggleterm.lua
@@ -1,10 +1,55 @@
 return {
   "akinsho/toggleterm.nvim",
   version = "*",
-  config = true,
-  -- keys = {
-  --   {
-  --     "<Leader>T",
-  --   },
-  -- },
+  config = function()
+    local toggleterm = require("toggleterm")
+    local Terminal = require("toggleterm.terminal").Terminal
+    toggleterm.setup({
+      size = function(term)
+        if term.direction == "horizontal" then
+          return 20
+        elseif term.direction == "vertical" then
+          return vim.o.columns * 0.4
+        end
+      end,
+      -- c-\ を押せば Terminal を toggle する
+      open_mapping = [[<c-\>]],
+      hide_numbers = true,
+    })
+
+    -- git が有効なときにだけ動く
+    local gituiTerminal = Terminal:new({
+      cmd = "gitui",
+      name = "gitui",
+      dir = "git_dir",
+      direction = "float",
+      hidden = true,
+    })
+    function __ToggleGitUITerminal()
+      gituiTerminal:toggle()
+    end
+    vim.api.nvim_set_keymap(
+      "n",
+      "<Leader>TG",
+      "<cmd>lua __ToggleGitUITerminal()<CR>",
+      { noremap = true, silent = true, desc = "gitui" }
+    )
+
+    -- c-\ でデフォルト terminal を出せるので defaultBottomTerminal は使わなそう
+    --[[ -- default
+    local defaultBottomTerminal = Terminal:new({
+      name = "default",
+      dir = "git_dir",
+      direction = "horizontal",
+    })
+    function __ToggleDefaultBottomTerminal()
+      defaultBottomTerminal:toggle()
+    end
+    vim.api.nvim_set_keymap(
+      "n",
+      "<Leader>TX",
+      "<cmd>lua __ToggleDefaultBottomTerminal()<CR>",
+      { noremap = true, silent = true, desc = "default" }
+    ) ]]
+  end,
 }
